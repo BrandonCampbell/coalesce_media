0c1ae50d446a68bd75f13a7a4841acb1dc63bb1d
afterburnertech <brandon@afterburnertechnologies.com>
2014-01-21 13:42:18 -0500

    basic structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CoalesceMedia::Application.config.secret_key_base = 'a4f6c6dd772dad6544a6b67244c5052eb7530b59f0e7eb1df2baa9f3d752d203fa80c5c7738472983878afb5b54de60bfb9a3048ef66e4df746d5dec8e4a9422'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CoalesceMedia::Application.config.session_store :cookie_store, key: '_coalesce_media_session'
